a9430d9e17d24c9c477c02f1feb702c8ebb71fe2
Valentina Bass Skrivanelli <aldanavalentinabass@gmail.com>
2026-07-22 18:51:17 -0300

    localstorage working

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,31 @@
-//Save info / save info when something happend
-import { useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import EventCard from "./components/EventCard";
 import BookingForm from "./components/BookingForm";
 
 function App() {
-   //creating the event cards for the events using props
-  const [events, setEvents] = useState([
+
+ const [events, setEvents] = useState(() => {
+
+  const storedEvents = localStorage.getItem("events");
+
+  if (storedEvents) {
+    return JSON.parse(storedEvents);
+  }
+
+  return [
     {
       id: 1,
       title: "1.1 MSA",
       date: "July 30, 2026",
       location: "Remote - Teams",
-      //Each event will have a variable indicating whether it was booked.
       booked: false
     },
-    {   
+    {
       id: 2,
       title: "1.1 Weekly Meeting",
       date: "July 27, 2026",
       location: "Remote - Teams",
-      //Each event will have a variable indicating whether it was booked.
       booked: false
     },
     {
@@ -28,48 +33,71 @@ function App() {
       title: "Career Development",
       date: "August 10, 2026",
       location: "Remote - Teams",
-      //Each event will have a variable indicating whether it was booked.
       booked: false
     }
-  ]);
-  
+  ];
+
+});
+
+
+  // Save events to localStorage
+  useEffect(() => {
+    localStorage.setItem("events", JSON.stringify(events));
+  }, [events]);
+
   function addEvent(newEvent) {
+
     const event = {
       id: events.length + 1,
+      booked: false,
       ...newEvent
     };
+
     setEvents([...events, event]);
   }
 
   function deleteEvent(id) {
-    const updatedEvents = events.filter((event) => event.id !== id);
+
+    const updatedEvents = events.filter(
+      (event) => event.id !== id
+    );
+
     setEvents(updatedEvents);
   }
 
   function toggleBooking(id) {
+
     const updatedEvents = events.map((event) => {
+
       if (event.id === id) {
-        return { ...event, booked: !event.booked };
+
+        return {
+          ...event,
+          booked: !event.booked
+        };
+
       }
+
       return event;
+
     });
+
     setEvents(updatedEvents);
   }
-
 
   return (
     <>
       <Header />
-       <BookingForm addEvent={addEvent} />
-      {/* Iterate through the events array and create an EventCard for each event */}
+
+      <BookingForm addEvent={addEvent} />
+
       {events.map((event) => (
         <EventCard
           key={event.id}
-          tit
-          le={event.title}
+          id={event.id}
+          title={event.title}
           date={event.date}
           location={event.location}
-          id={event.id}
           booked={event.booked}
           onDelete={deleteEvent}
           onToggleBooking={toggleBooking}
